7f32030351dc91324a23cd8fe5b8b3087aa500c8
Philippe Suter <psuter@us.ibm.com>
2016-10-12 16:40:49 -0400

    Skeleton

---
+++ run-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# A "build"
+
+echo "Building fancy project..."
+
+echo "$(date) : initializing..." >> "${LOGFILE}"
+echo "$(date) : done." >> "${LOGFILE}"
+
+echo "Build completed."
